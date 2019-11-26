24e6c27851aad6a5f8c095d64e3ca602eacb3398
Luis Flores Álvarez <luisfloresdev@gmail.com>
2019-11-26 11:01:34 -0600

    Add background in Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import '../assets/styles/components/Layout.scss';
 
 const Layout = ({ children }) => (
-  <>
+  <div className='layout'>
     <Header />
     {children}
-  </>
+  </div>
 );
 
 export default Layout;
